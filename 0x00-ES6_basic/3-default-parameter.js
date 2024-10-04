de6ab6fb6efa276179e240d1a35d9f4cc2ee34be
MohamedAlaga <malaga.142014@gmail.com>
2024-10-05 00:08:13 +0300

    3. Parameter defaults

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,9 +1,3 @@
 export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  // eslint-disable-next-line no-param-reassign
-  expansion1989 = expansion1989 || 89;
-  // eslint-disable-next-line no-param-reassign
-  expansion2019 = expansion2019 || 19;
-  // eslint-disable-next-line no-param-reassign
-  initialNumber = initialNumber || 0;
   return initialNumber + expansion1989 + expansion2019;
 }
