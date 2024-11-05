7f85e381047fe92d221bc86f86d7d05aa3efc5c5
Mohamed Alaga <malaga.142014@gmail.com>
2024-11-06 01:10:24 +0200

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,12 @@
-const groceriesList = () => {};
+const groceriesList = () => {
+  const grociry = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5]
+  ]);
+  return grociry;
+};
+
+export default groceriesList;
