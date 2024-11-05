8b42e890a2730148d9c6b246cccd15d970fdc8da
Mohamed Alaga <malaga.142014@gmail.com>
2024-11-06 01:11:35 +0200

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 const cleanSet = (set, startString) => {
   if (startString === '' || typeof startString !== 'string') return '';
-  const strings = Array.from(set)
+  return Array.from(set)
     .filter((str) => {
       return typeof str === 'string' && str.startsWith(startString);
     })
     .map((str) => str.replace(startString, ''))
     .join('-');
-  return strings;
+  
 };
 
 export default cleanSet;
