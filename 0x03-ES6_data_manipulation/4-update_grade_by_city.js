dc311ed199ae0517aa98542b6ebd7a797250a53f
Mohamed Alaga <malaga.142014@gmail.com>
2024-11-05 16:48:24 +0200

    4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,23 @@
+const updateStudentGradeByCity = (listStudents, city, newGrades) => {
+  let listStudentGrades = [];
+  if (!Array.isArray(listStudentGrades)) {
+    return listStudentGrades;
+  }
+  const studentWithCity = listStudents.filter(
+    (student) => student.location === city
+  );
+  listStudentGrades = studentWithCity.map((student) => {
+    newGrades.forEach((element) => {
+      if (element.studentId === student.id) {
+        student.grade = element.grade;
+      }
+    });
+    if (student.grade == null) {
+      student.grade = 'N/A';
+    }
+    return student;
+  });
+  return listStudentGrades;
+};
+
+export default updateStudentGradeByCity;
