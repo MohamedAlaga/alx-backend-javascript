a92cbf752f6aea8ab540eec2b3087ea2be7d1801
Mohamed Alaga <malaga.142014@gmail.com>
2024-11-05 18:18:12 +0200

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,7 +2,7 @@ const createInt8TypedArray = (length, position, value) => {
   const buffer = new ArrayBuffer(length);
   const view = new Uint8Array(buffer);
   view[position] = value;
-  return buffer;
+  return view;
 };
 
 export default createInt8TypedArray;
