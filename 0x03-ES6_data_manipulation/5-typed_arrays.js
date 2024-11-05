beb742ed19d0b5d5d4a0f3cdd8f2d373b24ed32f
Mohamed Alaga <malaga.142014@gmail.com>
2024-11-05 18:34:52 +0200

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,8 +1,9 @@
 const createInt8TypedArray = (length, position, value) => {
-    const buffer = new ArrayBuffer(length);
-    const view = new Uint8Array(buffer);
-    view[position] = value;
-    return new DataView(buffer);
+  const buffer = new ArrayBuffer(length);
+  const view = new Uint8Array(buffer);
+  if (position > Uint8Array.length) throw new Error("Position outside range");
+  view[position] = value;
+  return new DataView(buffer);
 };
 
 export default createInt8TypedArray;
