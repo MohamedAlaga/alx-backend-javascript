e26d2f6fb993973b235765bb12f2cef292675a48
Mohamed Alaga <malaga.142014@gmail.com>
2024-11-30 04:21:52 +0200

    full

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,24 @@
-// app that display a welcome message to the user
+/**
+ * Prompts the user to enter their name, reads the input from STDIN,
+ * and displays a message containing the user's name.
+ *
+ * When the user ends the program, a closing message is displayed.
+ *
+ * @example
+ * // Output:
+ * // Welcome to Holberton School, what is your name?
+ * // Alice
+ * // Your name is: Alice
+ * // This important software is now closing
+ */
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on("data", (data) => {
-  process.stdout.write(`Your name is: ${data}`);
+process.stdin.on("readable", () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
 process.stdin.on("end", () => {
