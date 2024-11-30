ff2e9c997c77485048f08036251b6db94d02c12f
Mohamed Alaga <malaga.142014@gmail.com>
2024-11-30 04:07:12 +0200

    1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,3 +4,7 @@ process.stdin.on("data", (data) => {
   process.stdout.write(`Your name is: ${data.toString()}`);
   process.exit();
 });
+
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
+});
