9cfc8a1ab1a7984d639c5211dacf41a87a219ae7
Mohamed Alaga <malaga.142014@gmail.com>
2024-11-30 04:34:24 +0200

    eslint fix

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,5 +1,5 @@
-export class AppController {
+export default class AppController {
   static getHomepage(req, res) {
-    res.status(200).send("Hello Holberton School!");
+    res.status(200).send('Hello Holberton School!');
   }
 }
