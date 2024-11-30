56294dca542ae7e4b03f93095cd7f0e59dfff366
Mohamed Alaga <malaga.142014@gmail.com>
2024-11-30 04:09:14 +0200

    1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,8 @@
 // app that display a welcome message to the user
-console.log("Welcome to Holberton School, what is your name?");
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
 process.stdin.on("data", (data) => {
-  process.stdout.write(`Your name is: ${data.toString()}`);
-  process.exit();
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on("end", () => {
